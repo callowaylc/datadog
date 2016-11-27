3a67fae9be14dfea20cb1184049fc2935a908f04
christian c <callowaylc@gmail.com>
2016-11-27 16:47:16 -0500

    Flags finished

--- options.go
+++ options.go
@@ -1,22 +1,39 @@
 package datadog_go_cli
 
 import(
-  _ "os"
+  "os"
+  _ "fmt"
 	flags "github.com/jessevdk/go-flags"
 )
 
 type Options struct {
-  Address string `short:"a" long:"address" description:"Statsd agent host:port" default:"127.0.0.1:8125"`
+  Address string `default:"127.0.0.1:8125" short:"a" long:"address" description:"statsd agent host:port" default:"127.0.0.1:8125"`
+  Name string `required:"true" short:"n" long:"name" description:"metric name"`
+  Type string `required:"true" short:"t" long:"type" description:"metric type ex. counter, histogram, gauge"`
+  Value interface{} `required:"true" short:"v" long:"value" description:"metric value for our given type"`
+  Tags []string `long:"tag" description:"metric tags"`
+
 }
 var o Options
 
 func InitOptions() {
-  flags.Parse(&o)
-
-  //if err != nil {
-  //  panic(err)
-  //  os.Exit(1)
-  //}
+  _, err := flags.Parse(&o)
+  
+  if flagsErr, ok := err.(*flags.Error); ok {
+    switch flagsErr.Type {
+    case flags.ErrHelp:
+      os.Exit(0)  
+    case flags.ErrTag:
+      panic(err)
+      os.Exit(1)
+    default:
+      os.Exit(2)
+    }
+  
+  } else if err != nil {
+    panic(err)
+    os.Exit(1)
+  }
 }
 
 func GetOptions() (Options, error) {
